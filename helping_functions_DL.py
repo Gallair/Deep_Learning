e6970f3128b50055d855ade6ece5e13a218588fc
Gallair <84108483+Gallair@users.noreply.github.com>
2025-04-25 10:02:05 -0500

    Update helping_functions_DL.py

--- helping_functions_DL.py
+++ helping_functions_DL.py
@@ -257,5 +257,47 @@ def print_base_model_layers(model_name,layer_index):
   for i, layer in enumerate(model_name.layers[layer_index].layers):
     print(i, layer.name, layer.trainable)
 
+def compare_histories(original_history, new_history, initial_epochs=5):
+  """
+  Compares two TensorFlow history objects
+  """
+  # get original history measurements
+  acc = original_history.history["accuracy"]
+  loss = original_history.history["loss"]
+
+  val_acc = original_history.history["val_accuracy"]
+  val_loss = original_history.history["val_loss"]
+
+  # combine original history metrics with new history metrics
+  total_acc = acc + new_history.history["accuracy"]
+  total_loss = loss + new_history.history["loss"]
+
+  total_val_acc = val_acc + new_history.history["val_accuracy"]
+  total_val_loss = val_loss + new_history.history["val_loss"]
+
+  # Make plots for accuracy
+  plt.figure(figsize=(8, 8))
+  plt.subplot(2, 1, 1)
+  plt.plot(total_acc, label='Training Accuracy')
+  plt.plot(total_val_acc, label='Validation Accuracy')
+
+  plt.plot([initial_epochs-1, initial_epochs-1],
+           plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+  plt.legend(loc='lower right')
+  plt.title('Training and Validation Accuracy')
+
+
+  # Make plots for loss
+  plt.figure(figsize=(8, 8))
+  plt.subplot(2, 1, 2)
+  plt.plot(total_loss, label='Training Loss')
+  plt.plot(total_val_loss, label='Validation Loss')
+
+  plt.plot([initial_epochs-1, initial_epochs-1],
+           plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+  plt.legend(loc='upper right')
+  plt.title('Training and Validation Loss')
+
+
 
 
